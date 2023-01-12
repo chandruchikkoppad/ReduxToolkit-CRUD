f1471f68c3227650370f7fe8c029243f2d9cbdeb
chandru chikkoppad <chandruchikkoppa@gmail.com>
2023-01-12 17:42:30 +0530

    reduxtoolkit placing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import UserPost from "./UserPost";
+import Create from "./UserPost/Create";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <h2>Understand Redux Toolkit Using API</h2>
+        <Routes>
+          <Route path="/"  element={<UserPost/>} />
+          <Route path="/create" element={<Create/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
